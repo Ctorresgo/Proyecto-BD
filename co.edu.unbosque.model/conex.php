321f8050720cdf39d665e54680727e3557e82003
Sebastian542 <sebastian.jsmopu@gmail.com>
2023-06-03 21:47:37 -0500

    Form

--- co.edu.unbosque.model/conex.php
+++ co.edu.unbosque.model/conex.php
@@ -17,8 +17,10 @@ class Db {
         $this->user = constant('DB_USER');
         $this->pass = constant('DB_PASS');
 
+
         try {
             $this->conection = new PDO('mysql:host='.$this->host.'; dbname='.$this->db, $this->user, $this->pass);
+            return conection ;
         } catch (PDOException $e) {
             echo $e->getMessage();
             exit();
